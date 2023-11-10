5abdfe281aa4116c87b860a78d3aa00b3029882f
Gabriel-v-Andreoli <gabrielvidalandreoli@gmail.com>
2023-11-10 08:35:02 -0300

    Trabalho concluido

--- src/grupoB/MergeSort.java
+++ src/grupoB/MergeSort.java
@@ -4,76 +4,72 @@ public class MergeSort {
     public int nDeTrocas;
     public int iteracoes;
 
-    public int tamanho(int[] vetor){
+    public int tamanho(int[] vetor) {
         int contador = 0;
-        while (true){
+        while (true) {
             try {
                 int elemento = vetor[contador];
                 contador++;
-            } catch (ArrayIndexOutOfBoundsException e){
+            } catch (ArrayIndexOutOfBoundsException e) {
                 break;
             }
         }
         return contador;
     }
 
-    public void sort(int[] vetor, int inicio, int fim){
-        if (fim - inicio > 1){
-            int meio = (fim + inicio)/2;
+    public void sort(int[] vetor, int inicio, int fim) {
+        if (fim - inicio > 1) {
+            int meio = (fim + inicio) / 2;
             sort(vetor, inicio, meio);
             sort(vetor, meio, fim);
-            merge(vetor, inicio, meio, fim);
-        }
-    }
-
-    public void merge(int[] vetor, int inicio, int meio, int fim){
-        int[] esquerda = new int[meio - inicio];
-        for (int i = 0; i < meio - inicio ; i++) {
-            esquerda[i] = vetor[i + inicio];
-            iteracoes++;
-        }
-        int[] direita = new int[fim - meio];
-        for (int i = meio; i < fim; i++) {
-            direita[i - meio] = vetor[i];
-            iteracoes++;
-        }
-        int topEsq = 0;
-        int topDir = 0;
-        for (int i = inicio; i < fim; i++) {
-            if (topEsq >= tamanho(esquerda)){
-                if (vetor[i] == direita[topDir]){
-                    topDir++;
-                } else {
-                    vetor[i] = direita[topDir];
-                    topDir++;
-                    nDeTrocas++;
-                }
-            } else if (topDir >= tamanho(direita)){
-                if (vetor[i] == esquerda[topEsq]){
-                    topDir++;
-                } else {
-                    vetor[i] = esquerda[topEsq];
-                    topEsq++;
-                    nDeTrocas++;
-                }
-            } else if (esquerda[topEsq] < direita[topDir]){
-                if (vetor[i] == esquerda[topEsq]){
-                    topEsq++;
-                }else {
-                    vetor[i] = esquerda[topEsq];
-                    topEsq++;
-                    nDeTrocas++;
-                }
-            } else {
-                if (vetor[i] == direita[topDir]){
-                    topEsq++;
-                }else {
-                    vetor[i] = direita[topDir];
-                    topDir++;
-                    nDeTrocas++;
-                }
+            int[] esquerda = new int[meio - inicio];
+            for (int i = 0; i < meio - inicio; i++) {
+                esquerda[i] = vetor[i + inicio];
+                iteracoes++;
             }
-            iteracoes++;
+            int[] direita = new int[fim - meio];
+            for (int i = meio; i < fim; i++) {
+                direita[i - meio] = vetor[i];
+                iteracoes++;
+            }
+            int topEsq = 0;
+            int topDir = 0;
+            for (int i = inicio; i < fim; i++) {
+                if (topEsq >= tamanho(esquerda)) {
+                    if (vetor[i] == direita[topDir]) {
+                        topDir++;
+                    } else {
+                        vetor[i] = direita[topDir];
+                        topDir++;
+                        nDeTrocas++;
+                    }
+                } else if (topDir >= tamanho(direita)) {
+                    if (vetor[i] == esquerda[topEsq]) {
+                        topDir++;
+                    } else {
+                        vetor[i] = esquerda[topEsq];
+                        topEsq++;
+                        nDeTrocas++;
+                    }
+                } else if (esquerda[topEsq] < direita[topDir]) {
+                    if (vetor[i] == esquerda[topEsq]) {
+                        topEsq++;
+                    } else {
+                        vetor[i] = esquerda[topEsq];
+                        topEsq++;
+                        nDeTrocas++;
+                    }
+                } else {
+                    if (vetor[i] == direita[topDir]) {
+                        topEsq++;
+                    } else {
+                        vetor[i] = direita[topDir];
+                        topDir++;
+                        nDeTrocas++;
+                    }
+                }
+                iteracoes++;
+            }
         }
     }
 }
